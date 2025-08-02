36be1753adec4aaea88a808389b2eef569e78a73
theGeniusDennis <danim005@st.ug.edu.gh>
2025-08-02 15:06:46 +0100

    chore: Readme relocated

--- ConsoleApplication/obj/Debug/net9.0/ConsoleApplication.AssemblyInfo.cs
+++ ConsoleApplication/obj/Debug/net9.0/ConsoleApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b18a7ec044e8c590f0269cb79494ac2fc6d161f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+075efb8f8b687edd583de7b04254f75da8fd447c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
